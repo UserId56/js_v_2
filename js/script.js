cafaeff2ac1cbe8c751847edeb8d9820d12b62bb
UserId56 <zaregay_menay@mail.ru>
2021-03-30 19:38:42 +0700

    Под украсил страницу

--- js/script.js
+++ js/script.js
@@ -3,13 +3,15 @@ const goodBlock = document.querySelector('.goods-list')
 const cartBlock = document.querySelector('.cart-list')
 
 class Item {
-    constructor(id, title, price) {
+    constructor(id, title, price, imgUrld, description) {
         this.id = id
         this.title = title;
         this.price = price;
+        this.imgUrld = imgUrld
+        this.description = description
     }
     render() {
-        return `<div class="goods-item" data-id="${this.id}"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+        return `<div class="goods-item" data-id="${this.id}"><h3>${this.title}</h3><img src="${this.imgUrld}"><span class="description">${this.description}</span><p>Цена: <span class="item-price">${this.price}</span></p><input type="number" name="count" id="item-${this.id}" class="item-count" value="1" min="1"><button class="add-item" type="button">В корзину</button></div>`;
     }
 }
 
@@ -18,7 +20,7 @@ class ListItems {
         this.list = [];
     }
 
-    fetch(url, list) {
+    fetch(url, list, itsCat) {
         fetch(url)
             .then((response) => response.json())
             .then((dateItems) => list.render(dateItems))
@@ -33,7 +35,7 @@ class GoodsItems extends ListItems {
         this.list = RequestListItems
         let listHtml = '';
         this.list.forEach(good => {
-            const goodItem = new Item(good.id, good.title, good.price);
+            const goodItem = new Item(good.id, good.title, good.price, good.imgUrl, good.description);
             listHtml += goodItem.render();
         });
         goodBlock.innerHTML = listHtml;
@@ -51,13 +53,13 @@ const list = new GoodsItems();
 list.fetch("goods/goods.json", list);
 
 class CartItem extends Item {
-    constructor(id, title, price, count) {
-        super(id, title, price)
+    constructor(id, title, price, imgUrl, count) {
+        super(id, title, price, imgUrl)
         this.count = count
         this.summ = CartItem.summItem(count, price)
     }
     render() {
-        return `<div class="cart-item" data-id="${this.id}"><h3>${this.title}</h3><p>${this.price}</p><p>Кол-во: ${this.count}</p><p>Сумма: ${this.summ}</p></div>`;
+        return `<div class="cart-item" data-id="${this.id}"><h3>${this.title}</h3><img src="${this.imgUrld}"><p>Цена:${this.price}</p><input type="number" name="count" id="item-${this.id}" class="item-count" value="${this.count}" min="1"><p>Сумма: ${this.summ}</p></div>`;
     }
     static summItem(count, price) {
         return count * price
@@ -73,7 +75,8 @@ class CartList extends ListItems {
         this.list = RequestListItems
         let listHtml = '';
         this.list.forEach(item => {
-            const cartItem = new CartItem(item.id, item.title, item.price, item.count);
+            let ind = list.list.findIndex((good) => good.id == item.id)
+            const cartItem = new CartItem(item.id, list.list[ind].title, list.list[ind].price, list.list[ind].imgUrl, item.count);
             listHtml += cartItem.render();
         });
         cartBlock.innerHTML = listHtml;
@@ -86,27 +89,44 @@ class CartList extends ListItems {
         return summerAllItem
     }
     addItemCart(id, count) {
+        let add = false
         this.list.forEach(item => {
             if (id == item.id) {
-                item.count = count
-            } else
-                indexItem = list.findindex(el => (id == el.id) ? true : false)
-            cartList.push(list[indexItem])
+                item.count += count
+
+            }
         })
+        if (!add) {
+            let ind = list.list.findIndex((item) => id == item.id)
+            console.log(ind)
+            cartList.list.push({
+                id: list.list[ind].id,
+                count: count
+            })
+            this.sendAddCart(cartList)
+        }
+    }
+    sendAddCart(list) {
+        fetch('cart/cart.json', {
+            method: "POST",
+            body: JSON.stringify(list)
+        }).catch((error) => {
+            console.log(error)
+        })
+
     }
 }
 
-function cartGen() {
-    let cartList = new CartList
-    cartList.fetch('cart/cart.json', cartList)
-}
+const cartList = new CartList
+cartList.fetch('cart/cart.json', cartList)
 
 function showCart() {
     cartBlock.classList.toggle("d-none")
     goodBlock.classList.toggle("d-none")
     if (cartBlock.className == "cart-list") {
-        cartGen()
-    }
+        cartList.fetch('cart/cart.json', cartList)
+        cartbtn.innerText = "Назад"
+    } else cartbtn.innerText = "Корзина"
 }
 
 cartbtn.addEventListener("click", showCart)
